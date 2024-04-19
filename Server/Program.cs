120d2ca4d09d062a02b2fcdfdfd657f547edfaea
Sinkarq <ebazier@outlook.com>
2024-04-19 21:15:26 +0300

    Program Initial Code

--- Server/Program.cs
+++ Server/Program.cs
@@ -1,5 +1,61 @@
+using FastEndpoints;
+using FastEndpoints.Swagger;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Server.Common.Infrastructure;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
+builder.Services.ConfigurePOCO<DatabaseOptions>(builder.Configuration);
+
+builder.Services
+    .AddLogging()
+    .AddCommonModule()
+    .AddCors(x => x.AddDefaultPolicy(y => y.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader()))
+    .AddDateOnlyTimeOnlyStringConverters()
+    .AddOutputCache()
+    .AddFastEndpoints(c => c.Assemblies = new[]
+    {
+        typeof(Program).Assembly,
+    })
+    .SwaggerDocument()
+    .AddAuthorization()
+    .AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+    .AddCookie(
+        CookieAuthenticationDefaults.AuthenticationScheme,
+        o => {
+            o.ExpireTimeSpan = TimeSpan.FromDays(1);
+            o.Cookie.MaxAge = TimeSpan.FromDays(1);
+            o.Cookie.HttpOnly = false;
+            o.Cookie.SameSite = SameSiteMode.Lax;
+        });
+        
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowSpecificOrigin",
+        policyBuilder => policyBuilder.WithOrigins("https://localhost:7040")
+            .AllowAnyHeader()
+            .AllowAnyMethod()
+            .AllowCredentials());
+});
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseCors("AllowSpecificOrigin");
+}
+
+app.UseAuthentication()
+    .UseAuthorization()
+    .UseOutputCache();
+
+app.UseFastEndpoints(c => {
+        c.Endpoints.RoutePrefix = "api";
+        c.Endpoints.Configurator = endpoints => {
+            //endpoints.DontThrowIfValidationFails();
+            //endpoints.PreProcessor<>();
+        };
+    })
+    .UseSwaggerGen();
+
+app.Run();

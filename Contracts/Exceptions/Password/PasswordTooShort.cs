43545fc7812e97243a1722a2d7df1cc33e5074f8
Sinkarq2 <82605720+Sinkarq@users.noreply.github.com>
2024-04-20 08:09:44 +0300

    Good to send the creation api

--- Contracts/Exceptions/Password/PasswordTooShort.cs
+++ Contracts/Exceptions/Password/PasswordTooShort.cs
@@ -7,5 +7,5 @@ public sealed class PasswordTooShort : IException
     public static PasswordTooShort Instance { get; } = new();
     public string ErrorMessage => "Паролата трябва да бъде поне 6 символа дълга.";
 
-    public static bool IsValid(string password) => password.Length > 8;
+    public static bool IsValid(string password) => password.Length > 6;
 }

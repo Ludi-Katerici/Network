fbff526a189c206a14fc704362a584dd30c34e22
Toma Andreev <86717095+Tomkata@users.noreply.github.com>
2024-04-20 12:59:01 +0300

    Added Create Event

---
+++ FrontEnd/Components/Pages/CreateEventFormInputModel.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FrontEnd.Components.Pages;
+
+internal sealed class CreateEventFormInputModel
+{
+    public string Name { get; set; }
+    
+    public string Region { get; set; }
+    
+    public string City { get; set; }
+    
+    public string Address { get; set; }
+    
+    public List<string> Categories { get; set; }
+    
+    public string Description { get; set; }
+    
+    [DataType(DataType.DateTime)]
+    public DateTime StartTime { get; set; } = DateTime.Now;
+}
--- FrontEnd/Infrastructure/SelectableItem.cs
+++ FrontEnd/Infrastructure/SelectableItem.cs
@@ -1,6 +1,6 @@
 namespace FrontEnd.Infrastructure;
 
-internal sealed class SelectableItem<T>
+public sealed class SelectableItem<T>
 {
     public SelectableItem(T value) => this.Value = value;
     public T Value { get; init; }

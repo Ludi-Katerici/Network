5423a54535b314801b5b74dc89e6655a0cb1898b
Sinkarq2 <82605720+Sinkarq@users.noreply.github.com>
2024-04-20 11:28:07 +0300

    Added AzureTextAnalytics.cs

---
+++ Server/AzureTextAnalytics.cs
@@ -0,0 +1,18 @@
+﻿using Azure;
+using Azure.AI.TextAnalytics;
+
+namespace Server;
+
+internal sealed class AzureTextAnalytics
+{
+    void AnalyzeText(string text)
+    {
+        string key = "0034de55083a4fe2aa95ca2260c631b9";
+        Uri endpoint = new Uri("https://sinanhack.cognitiveservices.azure.com/");
+
+        AzureKeyCredential credentials = new AzureKeyCredential(key);
+        TextAnalyticsClient textClient = new TextAnalyticsClient(endpoint, credentials);
+
+        Response<KeyPhraseCollection> keyPhrasesResponse = textClient.ExtractKeyPhrases(text, "bg");
+    }
+}
